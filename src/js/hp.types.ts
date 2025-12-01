ea15fa0a90b9b6fc2cb6fb7b2cdb1f73f46811cd
Nauel Gómez <ngomez@codiara.com>
2025-12-01 22:22:20 +0100

    hp character types 2

---
+++ src/js/hp.types.ts
@@ -0,0 +1,28 @@
+export interface HPCharacter {
+    id:               string;
+    name:             string;
+    alternate_names:  string[];
+    species:          string;
+    gender:           string;
+    house:            string;
+    dateOfBirth:      null;
+    yearOfBirth:      null;
+    wizard:           boolean;
+    ancestry:         string;
+    eyeColour:        string;
+    hairColour:       string;
+    wand:             Wand;
+    patronus:         string;
+    hogwartsStudent:  boolean;
+    hogwartsStaff:    boolean;
+    actor:            string;
+    alternate_actors: string[];
+    alive:            boolean;
+    image:            string;
+}
+
+export interface Wand {
+    wood:   string;
+    core:   string;
+    length: number;
+}
--- src/js/teams.ts
+++ src/js/teams.ts
@@ -6,7 +6,8 @@ type HPHouse = 'Gryffindor' | 'Slytherin' | 'Hufflepuff' | 'Ravenclaw';
 interface HPCharacterWand {
     wood: string;
     core: string;
-    length: string;
+    // length?: string; // puede ser undefined
+    length: number | null;
 }
 
 interface HPCharacter {
@@ -49,7 +50,7 @@ async function getCharacters() {
     // id | name | house
     // "9e3f7ce4-b9a7-4244-b709-dae5c1f1d4a8" | "Harry Potter" | "Gryffindor"
     console.table(
-        data.filter( i => i.hogwartsStudent ).map( (i) => ({ id: i.id, name: i.name, house: i.house, actor: i.actor, patronus: i.patronus }))
+        data.filter( i => i.hogwartsStudent ).map( (i) => ({ id: i.id, name: i.name, house: i.house, actor: i.actor, patronus: i.patronus, length: i.wand.length?.toFixed(0) }))
     );
 }
 

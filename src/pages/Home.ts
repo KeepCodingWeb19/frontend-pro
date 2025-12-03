6f1459f32ec4d97de614c56ce10b8ca0c5c8c04e
Nauel Gómez <ngomez@codiara.com>
2025-12-03 21:48:40 +0100

    todo 2 home ok

--- src/pages/Home.ts
+++ src/pages/Home.ts
@@ -1,5 +1,13 @@
+interface CountdownElements {
+    days:       HTMLElement;
+    hours:      HTMLElement;
+    minutes:    HTMLElement;
+    seconds:    HTMLElement;
+}
 
 class Home {
+
+    private coundownElements: CountdownElements | null = null;
 
     constructor() {
         console.log('Home Instanciado');
@@ -8,6 +16,9 @@ class Home {
 
     private bootstrap(): void {
         this.initializeElemets();
+        setInterval(() => {
+            this.printDateToElements();
+        }, 1000);
     }
 
     private initializeElemets(): void {
@@ -16,10 +27,16 @@ class Home {
         const minutes = document.getElementById('minutes');
         const seconds = document.getElementById('seconds');
 
-        const now = new Date();
         // non-null assertion OJO Con esto
         // days!.innerText = now.getDate().toString();
         if (!days || !hours || !minutes || !seconds ) return;
+        this.coundownElements = { days, hours, minutes, seconds };
+    }
+
+    private printDateToElements(): void {
+        if (!this.coundownElements) return;
+        const { days, hours, minutes, seconds } = this.coundownElements;
+        const now = new Date();
         days.innerText = now.getDate().toString();
         hours.innerText = now.getHours().toString();
         minutes.innerText = now.getMinutes().toString();

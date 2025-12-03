7c0ebbe405c39256b16e7285ec4a9522ecad9f81
Nauel Gómez <ngomez@codiara.com>
2025-12-03 22:35:59 +0100

    countdown works

--- src/pages/Home.ts
+++ src/pages/Home.ts
@@ -1,3 +1,5 @@
+import { DateTime } from 'luxon';
+
 interface CountdownElements {
     days:       HTMLElement;
     hours:      HTMLElement;
@@ -9,6 +11,9 @@ class Home {
 
     private coundownElements: CountdownElements | null = null;
 
+    private readonly TARGET_MONTH = 11; // En Date() los meses son en index 0, en Luxon los meses son en index 1
+    private readonly TARGET_DAY = 11;
+
     constructor() {
         console.log('Home Instanciado');
         this.bootstrap();
@@ -16,8 +21,57 @@ class Home {
 
     private bootstrap(): void {
         this.initializeElemets();
+        if (this.coundownElements) {
+            this.startCountdown();
+        }
+    }
+
+    private getNextEventDate(): DateTime {
+        const now = DateTime.now().setZone('Europe/Madrid');
+        let targetYear = now.year;
+
+        const eventThisYear = DateTime.local(
+            targetYear,
+            this.TARGET_MONTH,
+            this.TARGET_DAY,
+            { zone: 'Europe/Madrid' }
+        ).startOf('day');
+
+        if (now > eventThisYear) {
+            targetYear++;
+        }
+
+        return DateTime.local(
+            targetYear,
+            this.TARGET_MONTH,
+            this.TARGET_DAY,
+            { zone: 'Europe/Madrid' }
+        ).startOf('day');
+    }
+
+    private calculateTimeRemaning(target: DateTime): { days: number, hours: number, minutes: number, seconds: number } {
+        const now = DateTime.now().setZone('Europe/Madrid');
+        const diff = target.diff(now, ['days', 'hours', 'minutes', 'seconds']);
+
+        return {
+            days: Math.floor(diff.days),
+            hours: Math.floor(diff.hours),
+            minutes: Math.floor(diff.minutes),
+            seconds: Math.floor(diff.seconds),
+        }
+    }
+
+    private startCountdown(): void {
+        const targetDate = this.getNextEventDate();// es la fecha objetivo;
+        
         setInterval(() => {
-            this.printDateToElements();
+            
+            // Calcular distancia
+            const timeRemaning = this.calculateTimeRemaning(targetDate);
+
+            // Pintar distancia
+            this.updateCountdownDisplay(timeRemaning);
+
         }, 1000);
     }
 
@@ -33,14 +87,14 @@ class Home {
         this.coundownElements = { days, hours, minutes, seconds };
     }
 
-    private printDateToElements(): void {
+    private updateCountdownDisplay(timeRemaning: { days: number, hours: number, minutes: number, seconds: number } ): void {
         if (!this.coundownElements) return;
         const { days, hours, minutes, seconds } = this.coundownElements;
         const now = new Date();
-        days.innerText = now.getDate().toString();
-        hours.innerText = now.getHours().toString();
-        minutes.innerText = now.getMinutes().toString();
-        seconds.innerText = now.getSeconds().toString();
+        days.innerText = timeRemaning.days.toString();
+        hours.innerText = timeRemaning.hours.toString();
+        minutes.innerText = timeRemaning.minutes.toString();
+        seconds.innerText = timeRemaning.seconds.toString();
     }
 
     // TODO:

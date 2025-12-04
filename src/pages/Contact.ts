3b0693b370aa5d0480e15f3c8dbab5dd53782a2a
Nauel Gómez <ngomez@codiara.com>
2025-12-04 20:58:07 +0100

    Form validate done

--- src/pages/Contact.ts
+++ src/pages/Contact.ts
@@ -4,6 +4,8 @@ import { FormValidator } from '../lib/FormValidator';
 class Contact extends Page {
 
     private readonly validator: FormValidator;
+    private readonly formId: string = 'contactForm';
+    private formElement: HTMLFormElement | null = null;
 
     constructor() {
         super();
@@ -14,21 +16,86 @@ class Contact extends Page {
 
     bootstrap(): void | Promise<void> {
         console.log('Página Contact Cargada');
-        this.handleSubmit();
+        this.initializeElements();
+        this.listenSubmit();
+        this.listenRealTimeValidation();
     }
 
     destroy(): void | Promise<void> {
         console.log('Saliendo de la página');
     }
 
-    private handleSubmit() {
-        const form = document.getElementById('contactForm') as HTMLFormElement;
-        form?.addEventListener('submit', (event) => {
+    private initializeElements(): void {
+        this.formElement = document.getElementById(this.formId) as HTMLFormElement;
+    }
+
+    private listenSubmit(): void {
+        this.formElement?.addEventListener('submit', (event: Event) => {
             event.preventDefault();
-            const errors = this.validator.validateForm(form);
-            console.log(errors);
+            this.handleSubmit();
         });
     }
+
+    private listenRealTimeValidation(): void {
+        const fields = this.formElement?.querySelectorAll('input, textarea, select');
+
+        fields?.forEach(field => {
+            if (
+                field instanceof HTMLInputElement ||
+                field instanceof HTMLTextAreaElement ||
+                field instanceof HTMLSelectElement
+            ) {
+                // 
+                field.addEventListener('blur', () => {
+                    // este evento se disparara al perder el focus.
+                    // Podemos validar un elemento en concreto y pintar su error?
+                })
+                field.addEventListener('input', () => {
+                    if (field.validity.valid) {
+                        this.clearFieldError(field);
+                    }
+                })
+            }
+        })
+    }
+
+    private clearFieldError(field: HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement ): void {
+        const errorSpan = document.querySelector(`.${field.name}-error-message`) as HTMLSpanElement;
+        if (errorSpan) {
+            errorSpan.textContent = '';
+            errorSpan.classList.remove('show');
+        }
+        field.classList.remove('invalid');
+    }
+
+    private handleSubmit(): void {
+        if (!this.formElement) return;
+
+        const errors = this.validator.validateForm(this.formElement);
+        const isValid = errors.size === 0;
+        
+        if (!isValid) {
+            errors.forEach((message, fieldName) => {
+                const field = this.formElement?.querySelector(`[name=${fieldName}]`) as HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement;
+                if (field) {
+                    this.showFieldError(field, message);
+                }
+
+            });
+        }
+    }
+
+    private showFieldError(field: HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement, message: string): void {
+        const errorSpan = document.querySelector(`.${field.name}-error-message`) as HTMLSpanElement;
+        if (errorSpan) {
+            errorSpan.textContent = message;
+            errorSpan.classList.add('show');
+        }
+        field.classList.add('invalid');
+    }
+
+    // TODO: en caso de error, mostrar el error en su label correspondiente
+    // OJO: hay que añadir la clase show
 
 }
 new Contact();
